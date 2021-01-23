10b4d1e8f069feb5b0d77fb74f5c9eadc8eb0578
WeebNetsu <stephenvdw7777@gmail.com>
2021-01-23 19:52:10 +0200

    Code Updates
    Code now follows the correct way of using DiscordPy

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
 # Personal Assistant
-Personal Assistant built in Python
+
+Personal Assistant Discord bot built in DiscordPy
 
 ## About
-This CLI (at the moment) application is a project I'm doing along with my subscribers on YouTube while live streaming. You can join live streams by subscribing <a href="https://www.youtube.com/stevesteacher" target="_blank">right here</a>. If you'd like to see the development, you can watch the live streams after the actual stream.
+
+This CLI (at the moment) application is a project I'm doing along with my subscribers on YouTube while live streaming. You can join live streams by subscribing <a href="https://www.youtube.com/stevesteacher">right here</a>. If you'd like to see the development, you can watch the live streams after the actual stream.
 
 ## Next live stream
-<b>Saturday, 16 Jan 2021, 18:00</b> (probably)
 
-## Links to previous live streams
-<p class="part">Part 1</p>
-<div class="links">
-<ul?>
-    <li>
-        <a href="https://youtu.be/-RN57Fe8Kk8" target="_blank">Part 1.1</a>
-    </li>
-    <li>
-        <a href="https://youtu.be/HHSi68d_ik0" target="_blank">Part 1.2</a>
-    </li>
-    <li>
-        <a href="https://youtu.be/Hy5Ms8g2ml4" target="_blank">Part 1.3</a>
-    </li>
-    <li>
-        <a href="https://youtu.be/JIyqiC8pLfA" target="_blank">Part 1.4</a>
-    </li>
-</ul>
-</div>
+<b>Saturday, 30 Jan 2021, 18:00</b> (probably)
+
+## Watch previous live streams
+
+<b><a href="https://youtube.com/playlist?list=PLYBJzqz8zpWb-vS6tjSPTTlg5Nlv8WFHd">Playlist</a></b>
--- main.py
+++ main.py
@@ -1,38 +1,87 @@
-# import client_details as c
-import discord, logging, json
+import discord  # to connect to discord and crap
+from discord.ext import commands
+import logging  # for simple logging
+import json  # to read the config.json file
+import requests
 
-logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
+# from pathlib import Path  # for paths (not using OS, but could)
 
-tFile = open("config.json", "r")  # opens file
-config = tFile.read()  # reads the entire file at once
-tFile.close()  # closes the file
-config = json.loads(config)
+# read json file and get bot details
+bot_details = open("config.json", "r")
+# creates a dict from the json it just read
+config = json.loads(bot_details.read())
+bot_details.close()
 
-# print(config["prefix"])
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+# create the bot
+bot = commands.Bot(command_prefix=config["prefix"], case_insensitive=True)
+bot.config_token = config["token"]
+logging.basicConfig(level=logging.INFO)
 
 
-@client.event
-async def on_message(message):
-    if (message.author == client.user) or not (
-        message.content.endswith(config["postfix"])
-    ):
-        return
+@bot.event
+async def on_ready():  # when connected and ready to go
+    print(f"----- Logged in as {bot.user.name} : {bot.user.id} -----")
+    # change bot activity
+    await bot.change_presence(activity=discord.Game(name="Chat with me, please :)"))
+
+
+@bot.command(name="hi", aliases=["hello"])
+# by default, the below will be triggered by !_hi, but since we made the name "hi" and give aliases as "hello"
+# the below will only be triggered by !hi and !hello
+async def _hi(ctx):  # _hi so you don't overwrite an existing function
+    await ctx.send(f"Hi, {ctx.author.mention}!")
+
+
+@bot.command()
+# the below code will be triggred when someone says !echo
+# without the * the message will be a tuple and not a string
+async def echo(ctx, *, message=None):  # !echo
+    message = message or "Please provide a message to copy"
+    await ctx.message.delete()  # deletes users original message
+    await ctx.send(f"{message}!")  # sends the message in their place
+
+
+weather_api_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+@bot.command(name="weather")
+async def _weather(ctx, *, message=None):
+    message = message or "Look out the windows"
+    complete_url = (
+        weather_api_url + "appid=" + config["weather_api_key"] + "&q=" + message
+    )
+    response = requests.get(complete_url)
+    x = response.json()
+    if x["cod"] != "404":
+        y = x["main"]
+        print(x, y)
+        celsius = round(y["temp"] - 273.15)
+        # (0°C × 9/5) + 32
+        fahrenheit = round(celsius * 9 / 5) + 32
+        formatted_resonse = f"Weather: {x['weather'][0]['description']}\n"
+        formatted_resonse += (
+            f"Temperature: {celsius}{chr(176)}C ({fahrenheit}{chr(176)}F)\n"
+        )
+
+        celsius = round(y["temp_max"] - 273.15)
+        fahrenheit = round(celsius * 9 / 5) + 32
+        formatted_resonse += (
+            f"Max Temperature: {celsius}{chr(176)}C ({fahrenheit}{chr(176)}F)\n"
+        )
+
+        celsius = round(y["temp_min"] - 273.15)
+        fahrenheit = round(celsius * 9 / 5) + 32
+        formatted_resonse += (
+            f"Min Temperature: {celsius}{chr(176)}C ({fahrenheit}{chr(176)}F)\n"
+        )
+
+        await ctx.send(
+            # f"{celsius}{chr(176)}C ({fahrenheit}{chr(176)}F)"
+            formatted_resonse
+        )  # sends the message in their place
     else:
-        # if message.content.startswith("$hello"):
-        #     await message.channel.send("Hello!")
-        print(message.content)
+        print(x)
+        await ctx.send(f"Could not get data!")
 
 
-client.run(config["token"])
+bot.run(bot.config_token)
